9594ed4a16d7f4dc0160db9cca66deb10c91a153
Shane Barys <shanejbarys@gmail.com>
2020-01-13 17:39:13 -0500

    Initial Commit

---
+++ Week1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Week1
+//
+//  Created by Shane Barys on 1/13/20.
+//  Copyright © 2020 Shane Barys. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
