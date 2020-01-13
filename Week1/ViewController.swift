c1e984e71e5348dc04bd093bd72126d50afef8a4
Shane Barys <shanejbarys@gmail.com>
2020-01-13 18:38:14 -0500

    first

--- Week1/ViewController.swift
+++ Week1/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func clickForMorePressed(_ sender: UIButton) {
+        messageLabel.text = "WOWZERS"
+    }
+    
 }
 
